6be1d29589732ab9e908fa67db6a4ce6cb73bf1e
devSeyf <ssaaiiffjjaammaall@gmail.com>
2026-02-27 16:12:42 +0300

    Add Todo List UI components and update dependencies

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
 export default function App() {
+  const [todo, setTodo] = React.useState("");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Todo List</Text>
+        <Text style={styles.headerSimpleText}>Simple Todo List</Text>
+      </View>
+      <View style={styles.containerInout}>
+        <TextInput
+          placeholder="Add a To-Do"
+          style={styles.input}
+          value={todo}
+          onChangeText={(t) => {
+            setTodo(t);
+          }}
+        />
+        <TouchableOpacity style={styles.containerButton}>
+          <Text style={styles.addButton}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +35,52 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+  },
+  headerText: {
+    color: "#ea8912",
+    fontSize: 50,
+    fontWeight: "bold",
+  },
+
+  headerSimpleText: {
+    color: "#fff",
+    fontSize: 15,
+    fontWeight: "bold",
+    marginTop: 15,
+  },
+  header: {
+    marginTop: 50,
+    marginBottom: 10,
+    alignItems: "center",
+  },
+
+  input: {
+    backgroundColor: "#fff",
+    height: 50,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    flex: 1,
+    borderRadius: 20,
+  },
+
+  containerInout: {
+    flexDirection: "row",
+  },
+  containerButton: {
+    backgroundColor: "#ea8912",
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
+  },
+
+  addButton: {
+    color: "#fff",
+    fontSize: 30,
+    fontWeight: "bold",
   },
 });
